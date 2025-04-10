ce69c382b74caf38a6a3dff31f4f5da8c333431d
Sajeelsahil1 <thegeniusresearch0@gmail.com>
2025-04-10 18:19:00 +0500

    Delete test_register.py

--- test_register.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "http://127.0.0.1:5000/register"
-files = {'file': open("p1.jpg", 'rb')}
-data = {'username': 'test_user'}
-
-response = requests.post(url, files=files, data=data)
-print("Response Status Code:", response.status_code)
-print("Response Text:", response.text)
